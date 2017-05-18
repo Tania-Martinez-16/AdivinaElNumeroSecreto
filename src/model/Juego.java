d35390d2ab8d0dd1e81d813d3ee29abb7a81cf75
Tania Georgina Martinez Tuz <16470257@itcampeche.edu.mx>
2017-05-17 20:29:35 -0500

    Primera versión completa del proyecto.

--- src/model/Juego.java
+++ src/model/Juego.java
@@ -10,5 +10,66 @@ package model;
  * @author tatia
  */
 public class Juego {
+    // Variables de instancia
+    private NumeroSecreto numeroSecreto;
+    private int limiteInferior;
+    private int limiteSuperior;
+    private int numeroDeIntentos;
+    private int intentosRealizados;
+    
+    // Constantes simbólicas
+    private static final int LIM_INF_PREDET = 1;
+    private static final int LIM_SUP_PREDET = 100;
+    
+    // Método setRangodeNumeroSecreto()
+    public void setRangoDelNumeroSecreto(int limiteInferior, int limiteSuperior){
+        this.limiteInferior = limiteInferior;
+        this.limiteSuperior = limiteSuperior;
+        numeroSecreto.setRango(this.limiteInferior, this.limiteSuperior);
+    }
+    
+    // Constructor
+    public Juego(int limiteInferior, int limiteSuperior, int intentos){
+        this.limiteInferior = limiteInferior;
+        this.limiteSuperior = limiteSuperior;
+        this.numeroDeIntentos = intentos;
+        this.numeroSecreto = new NumeroSecreto(this.limiteInferior, this.limiteSuperior);
+        this.intentosRealizados = 0;
+    }
+    
+    // Método Setter
+    public void setNumeroDeIntentos(int intentos){
+        this.numeroDeIntentos = intentos;
+    }
+    
+    // Método esElNumeroSecreto()
+    public boolean esElNumeroSecreto(int numero){
+        this.intentosRealizados++;
+        return numeroSecreto.esIgual(numero);
+    }
+    
+    // Método puedeIntentarDeNuevo()
+    public boolean puedeIntentarDeNuevo(){
+        boolean hayMasOportunidades = this.intentosRestantes() > 0;
+        return hayMasOportunidades;
+    }
+    
+    // Método intentosRestantes()
+    public int intentosRestantes(){
+        int numeroDeIntentosRestantes = this.numeroDeIntentos - this.intentosRealizados;
+        return(numeroDeIntentosRestantes);
+    }
+    
+    // Método getTextoDeAyuda()
+    public String getTextoDeAyuda(int numero){
+        String texto = null;
+        if (this.numeroSecreto.esMayor(numero) == true){
+            texto = "mayor";
+        }
+        else if (this.numeroSecreto.esMenor(numero) == true){
+            texto = "menor";
+        }
+        return texto;
+    }
     
 }
